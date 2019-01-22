3ae876bca39ab213360b55a1ac5ece3b8472e35f
ShadVlad <wowchik.shadrin@yandex.ru>
2019-01-22 22:35:11 +0300

    задача 3 (easy)

--- lesson04/home_work/hw04_easy.py
+++ lesson04/home_work/hw04_easy.py
@@ -28,10 +28,15 @@ assorti = [fruit for fruit in fruits if fruit in citruss]
 print('assorti: ', assorti,'\n')
 
 
-
 # Задание-3:
 # Дан список, заполненный произвольными числами.
 # Получить список из элементов исходного, удовлетворяющих следующим условиям:
 # + Элемент кратен 3
 # + Элемент положительный
 # + Элемент не кратен 4
+
+print('    -= Задача-3 =-\n')
+numbers = [2, 10, -12, 15, 20, -9, 4, 6, 0]
+print(numbers)
+numbers_gen = [el for el in numbers if int(el) % 3 == 0 and int(el) > 0 and int(el) % 4 != 0]
+print(numbers_gen)
