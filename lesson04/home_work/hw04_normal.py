211e9d37577db977f56fba94937aebf67ae4aea1
ShadVlad <wowchik.shadrin@yandex.ru>
2019-01-23 22:25:33 +0300

    Решено задание 04 (normal)

--- lesson04/home_work/hw04_normal.py
+++ lesson04/home_work/hw04_normal.py
@@ -1,24 +1,54 @@
+import re
+import random
+import os
 # Задание-1:
 # Вывести символы в нижнем регистре, которые находятся вокруг
 # 1 или более символов в верхнем регистре.
 # Т.е. из строки "mtMmEZUOmcq" нужно получить ['mt', 'm', 'mcq']
 # Решить задачу двумя способами: с помощью re и без.
 
+print("\n    -= Задание 1 =-")
+print("Поиск подстрок по шаблону")
+
 line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
-       'GIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLeclMwAoktKlfUBJHPsnawvjPhfgewVzK'\
-       'TUfSYtBydXaVIpxWjNKgXANvIoumesCSSvjEGRJosUfuhRRDUuTQwLlJJJDdkVjfSAHqn'\
-       'LxooisBDWuxIhyjJaXDYwdoVPnsllMngNlmkpYOlqXEFIxPqqqgAWdJsOvqppOfyIVjXa'\
-       'pzGOrfinzzsNMtBIOclwbfRzytmDgEFUzxvZGkdOaQYLVBfsGSAfJMchgBWAsGnBnWete'\
-       'kUTVuPluKRMQsdelzBgLzuwiimqkFKpyQRzOUyHkXRkdyIEBvTjdByCfkVIAQaAbfCvzQ'\
-       'WrMMsYpLtdqRltXPqcSMXJIvlBzKoQnSwPFkapxGqnZCVFfKRLUIGBLOwhchWCdJbRuXb'\
-       'JrwTRNyAxDctszKjSnndaFkcBZmJZWjUeYMdevHhBJMBSShDqbjAuDGTTrSXZywYkmjCC'\
-       'EUZShGofaFpuespaZWLFNIsOqsIRLexWqTXsOaScgnsUKsJxiihwsCdBViEQBHQaOnLfB'\
-       'tQQShTYHFqrvpVFiiEFMcIFTrTkIBpGUflwTvAzMUtmSQQZGHlmQKJndiAXbIzVkGSeuT'\
-       'SkyjIGsiWLALHUCsnQtiOtrbQOQunurZgHFiZjWtZCEXZCnZjLeMiFlxnPkqfJFbCfKCu'\
-       'UJmGYJZPpRBFNLkqigxFkrRAppYRXeSCBxbGvqHmlsSZMWSVQyzenWoGxyGPvbnhWHuXB'\
-       'qHFjvihuNGEEFsfnMXTfptvIOlhKhyYwxLnqOsBdGvnuyEZIheApQGOXWeXoLWiDQNJFa'\
-       'XiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQoiQ'\
+       'GIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLeclMwAoktKlfUBJHPsnawvjPhfgewVzK' \
+       'TUfSYtBydXaVIpxWjNKgXANvIoumesCSSvjEGRJosUfuhRRDUuTQwLlJJJDdkVjfSAHqn' \
+       'LxooisBDWuxIhyjJaXDYwdoVPnsllMngNlmkpYOlqXEFIxPqqqgAWdJsOvqppOfyIVjXa' \
+       'pzGOrfinzzsNMtBIOclwbfRzytmDgEFUzxvZGkdOaQYLVBfsGSAfJMchgBWAsGnBnWete' \
+       'kUTVuPluKRMQsdelzBgLzuwiimqkFKpyQRzOUyHkXRkdyIEBvTjdByCfkVIAQaAbfCvzQ' \
+       'WrMMsYpLtdqRltXPqcSMXJIvlBzKoQnSwPFkapxGqnZCVFfKRLUIGBLOwhchWCdJbRuXb' \
+       'JrwTRNyAxDctszKjSnndaFkcBZmJZWjUeYMdevHhBJMBSShDqbjAuDGTTrSXZywYkmjCC' \
+       'EUZShGofaFpuespaZWLFNIsOqsIRLexWqTXsOaScgnsUKsJxiihwsCdBViEQBHQaOnLfB' \
+       'tQQShTYHFqrvpVFiiEFMcIFTrTkIBpGUflwTvAzMUtmSQQZGHlmQKJndiAXbIzVkGSeuT' \
+       'SkyjIGsiWLALHUCsnQtiOtrbQOQunurZgHFiZjWtZCEXZCnZjLeMiFlxnPkqfJFbCfKCu' \
+       'UJmGYJZPpRBFNLkqigxFkrRAppYRXeSCBxbGvqHmlsSZMWSVQyzenWoGxyGPvbnhWHuXB' \
+       'qHFjvihuNGEEFsfnMXTfptvIOlhKhyYwxLnqOsBdGvnuyEZIheApQGOXWeXoLWiDQNJFa' \
+       'XiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQoiQ' \
        'zTYwZAiRwycdlHfyHNGmkNqSwXUrxGc'
+
+print("Исходная строка\n", line)
+str_low = ''
+str_upp = ''
+str_out = ''
+string_out = []
+lower = True
+
+for symbol in line:
+    if symbol.islower():
+        if lower:
+            str_out = str_out + symbol
+        else:
+            if len(str_out) > 0:
+                string_out.append(str_out)
+            str_out = symbol
+            lower = True
+    else:
+        lower = False
+print(string_out, '\n')
+
+print("То же с применением re")
+pattern = '[a-z]+'
+print(re.findall(pattern, line))
 
 
 # Задание-2:
@@ -28,6 +58,9 @@ line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
 # "GAMkgAYEOmHBSQsSUHKvSfbmxULaysmNOGIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLec"
 # нужно получить список строк: ['AY', 'NOGI', 'P']
 # Решить задачу двумя способами: с помощью re и без.
+
+print("\n    -= Задание 2 =-")
+print("Поиск подстрок по шаблону 2")
 
 line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm'\
        'NOGIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLeclMwAoktKlfUBJHPsnawvjPhfgewV'\
@@ -45,9 +78,74 @@ line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm'\
        'JFaXiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQ'\
        'oiQzTYwZAiRwycdlHfyHNGmkNqSwXUrxGC'
 
+str_lo = ''
+str_up = ''
+str_ou = ''
+string_ou = []
+lower1 = True
+upper = False
+for symbol in line_2:
+    if symbol.islower():
+        if lower1:
+            str_lo = str_lo + symbol
+        else:
+            if len(str_lo) >= 2 and len(str_up) >= 3:
+                string_ou.append(str_up[:-2])
+            str_lo = symbol
+            lower1 = True
+
+    else:
+        if lower1:
+            # if len(str_lo) == 2 and len(str_up) > 3:
+            #     string_ou.append(str_up[:-2])
+            str_up = symbol
+            lower1 = False
+        else:
+            str_up = str_up + symbol
+
+print("Исходная строка\n", line_2)
+print(line_2)
+print(string_ou, '\n')
+
+print("То же с применением re")
+pattern = '[a-z]{2}([A-Z]+)[A-Z]{2}'
+print(re.findall(pattern, line_2))
+
 # Задание-3:
 # Напишите скрипт, заполняющий указанный файл (самостоятельно задайте имя файла)
 # произвольными целыми цифрами, в результате в файле должно быть
 # 2500-значное произвольное число.
 # Найдите и выведите самую длинную последовательность одинаковых цифр
 # в вышезаполненном файле.
+
+print("\n    -= Задание 3 =-")
+print("Поиск максимальной подстроки из одинаковых цифр")
+
+DIR = "data"
+path = "data"
+
+if not os.path.exists(path):
+    try:
+        os.mkdir(path)
+    except OSError:
+        print(f"Создать директорию '{path}' не удалось")
+    else:
+        print(f"Успешно создана директория '{path}'")
+
+name_f = 'myfile.txt'
+with open(os.path.join(DIR, name_f), 'w', encoding='UTF-8') as my_file:
+    for _ in range(1250):
+        my_file.write(str(random.randint(0, 9)) * 2)
+        # для проверки обработки сделал сразу по два символа
+with open(os.path.join(DIR, name_f), 'r', encoding='UTF-8') as my_file:
+    longest_sequences = list()
+    numbers = my_file.read()
+    print("Исходное число:")
+    print(numbers, '\n')
+
+    for number in range(0, 9):
+        if str(number) in numbers:
+            lst = re.findall('[%s]+' % number, numbers)
+            longest_sequences.append(max(lst, key=len))
+    result = max(longest_sequences, key=len)
+    print("Максимальная последовательность одинаковых цифр: ", result)
